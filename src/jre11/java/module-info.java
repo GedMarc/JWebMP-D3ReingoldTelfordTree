ba68c2c557b656ad8e448844d30a1725f2b759e2
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:13:45 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.d3.implementations.D3ReingoldTreeExclusionsModule;
-import com.jwebmp.plugins.d3.radialreingoldtilfordtree.D3ReingoldTilfordTreePageConfigurator;
-
 module com.jwebmp.plugins.d3.reingoldtilfordtree {
 	exports com.jwebmp.plugins.d3.reingoldtilfordtree;
 	exports com.jwebmp.plugins.d3.radialreingoldtilfordtree;
@@ -15,10 +9,10 @@ module com.jwebmp.plugins.d3.reingoldtilfordtree {
 	requires java.validation;
 	requires com.jwebmp.guicedinjection;
 
-	provides IPageConfigurator with D3ReingoldTilfordTreePageConfigurator;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.d3.radialreingoldtilfordtree.D3ReingoldTilfordTreePageConfigurator;
 
-	provides IGuiceScanJarExclusions with D3ReingoldTreeExclusionsModule;
-	provides IGuiceScanModuleExclusions with D3ReingoldTreeExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.d3.implementations.D3ReingoldTreeExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.d3.implementations.D3ReingoldTreeExclusionsModule;
 
 	opens com.jwebmp.plugins.d3.radialreingoldtilfordtree to com.fasterxml.jackson.databind, com.jwebmp.core;
 	opens com.jwebmp.plugins.d3.reingoldtilfordtree to com.fasterxml.jackson.databind, com.jwebmp.core;
